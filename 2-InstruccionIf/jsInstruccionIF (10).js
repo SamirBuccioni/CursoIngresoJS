502c92d1f507fce820a2c45479865c029aceabaa
SamirBuccioni <60615422+SamirBuccioni@users.noreply.github.com>
2020-02-11 21:24:35 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,17 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var numero;
 
+	numero = Math.round(Math.random()*10);
+
+	if (numero == 10 || numero == 9){
+		alert("EXCELENTE");
+	}
+	else if (numero >= 4) {
+		alert("APROBÓ");
+	}
+	else {
+		alert("Vamos, la proxima se puede");
+	}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,5 +2,10 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var numero;
+
+	numero = Math.round(Math.random()*10);
+
+	alert(numero);
 
 }//FIN DE LA FUNCIÓN
